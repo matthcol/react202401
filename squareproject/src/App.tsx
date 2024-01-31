9ccac1c015ca8a01b26bd7d7e854a388363f8275
matthcol <matthias.colin@gmail.com>
2024-01-31 13:56:22 +0100

    change size

--- squareproject/src/App.tsx
+++ squareproject/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { initSquare } from './magic';
 import { IValueNumber } from './types/ivaluenumber';
+import { initStateGame, stateGameReducer } from './magicreducer';
 
 
 //function Square(props: {value: number, n: number}){
@@ -19,12 +20,57 @@ function SquareInfo({value, n}: IValueNumber){ // NB: return type =  JSX.Element
   </div>);
 }
 
+
+function Game() {
+  const [stateGame, dispatch] = useReducer(stateGameReducer, 3, initStateGame);
+  return (
+    <div className="game">
+      <h2>Game</h2>
+      <div className="controls">
+        <div>
+          Size
+          <input 
+              type="number" 
+              value={stateGame.n} 
+              onChange={(event) => dispatch(({
+                'type': 'resize_square',
+                'newN': parseInt(event.target.value, 10)
+              }))} 
+          />
+        </div>
+        <div>
+          Display sums
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              // setIsDisplaySum(e.target.checked);
+              // WARNING: following code don't trigger a render
+              // square[0][0]++;
+              // console.log(square);
+            }}
+          />
+        </div>
+      </div>
+      <div>
+        <ul>
+          <li>display: {stateGame.displaySums ? 'visible':'hidden'}</li>
+          <li>square size n: {stateGame.n}</li>
+          <li>number of values: {stateGame.values.length}</li>
+        </ul>
+      </div>
+    </div>
+);
+}
+
 function App() {
-  const square: number[][] = initSquare(3);
-  square[0][1] = 2;
+  // const square: number[][] = initSquare(3);
+  // square[0][1] = 2;
   return (
     <div className="App">
-      <SquareInfo n={3} value={square[0][1]}/>
+      { 
+        // <SquareInfo n={3} value={square[0][1]}/> 
+      }
+      <Game />;
     </div>
   );
 }
